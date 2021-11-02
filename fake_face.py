32c7e87f659f3833987adf3b6263e6f890595c0c
2Coolge <83459083+2Coolge@users.noreply.github.com>
2021-11-02 16:03:11 +0500

    Add files via upload

---
+++ fake_face.py
@@ -0,0 +1,121 @@
+from __future__ import division, print_function
+
+import os
+
+
+#mfor model
+import numpy as np
+
+from tensorflow.keras.layers import Input, Dense, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Dropout, Reshape, Concatenate, LeakyReLU
+
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing import image
+import tensorflow as tf
+
+# Keras
+
+from keras.models import load_model
+#from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+import urllib.request
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+file_id = '1YEMGDGKu6eCWnBALD0cs9dhRerVZINVS'
+destination= 'models/TB_model.h5'
+
+
+
+MODEL_PATH = 'models\inception_tb.h5'
+model_TB = tf.keras.models.load_model(MODEL_PATH)
+
+image_dimensions = {'height':256, 'width':256, 'channels':3}
+
+class Classifier:
+    def __init__(self):
+        self.model = 0
+
+    def predict(self, x):
+        return self.model.predict(x)
+
+    def fit(self, x, y):
+        return self.model.train_on_batch(x, y)
+
+    def get_accuracy(self, x, y):
+        return self.model.test_on_batch(x, y)
+
+    def load(self, path):
+        self.model.load_weights(path)
+
+
+
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    #x = preprocess_input(x, mode='caffe')
+
+    preds = model.predict(x)
+    # result = np.round(preds, 1)
+    result = np.argmax(preds, axis=1)
+    str = ""
+    
+    if result == 0:
+        str = "abnormal"
+    else:
+        str = "normal"
+
+    print(result)
+    print(preds)
+    return str
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model_TB)
+        print(preds)
+        os.remove(file_path)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        #pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        #result = str(pred_class[0][0][1])               # Convert to string
+        return preds
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
